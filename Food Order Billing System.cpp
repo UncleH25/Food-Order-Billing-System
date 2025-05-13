f77a4d9aa22e061adf66ae35e2461ab868fc97fa
UncleH25 <oneilljarryd@gmail.com>
2025-05-13 16:23:24 +0200

    Declared initial variables.

--- Food Order Billing System.cpp
+++ Food Order Billing System.cpp
@@ -5,6 +5,18 @@ using namespace std;
 
 int main()
 {
+	//Variables
+	const double PASTA_PRICE = 54.50;
+	const double BURGER_PRICE = 45.00;
+	const double PIZZA_PRICE = 60.00;
+	const double SANDWICH_PRICE = 30.00;
+	const double FRIES_PRICE = 20.00;
+	const double DRINK_PRICE = 15.00;
+
+	int menuChoice;
+	int quantity;
+	double price = 0.0;
+	double total = 0.0;
     
     return 0;
 }
