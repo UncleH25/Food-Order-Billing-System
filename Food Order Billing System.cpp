c4101606cf6a9ce81656000d7b93ae70915c052b
UncleH25 <oneilljarryd@gmail.com>
2025-05-13 16:25:15 +0200

    Added code to show the menu on startup.

--- Food Order Billing System.cpp
+++ Food Order Billing System.cpp
@@ -17,6 +17,17 @@ int main()
 	int quantity;
 	double price = 0.0;
 	double total = 0.0;
+
+	//Display menu
+	cout << "Welcome to the Food Order Billing System!" << endl;
+	cout << "Our Menu" << endl;
+	cout << "----------------------------------------" << endl;
+	cout << "1. Pasta - R" << fixed << setprecision(2) << PASTA_PRICE << endl;
+	cout << "2. Burger - R" << fixed << setprecision(2) << BURGER_PRICE << endl;
+	cout << "3. Pizza - R" << fixed << setprecision(2) << PIZZA_PRICE << endl;
+	cout << "4. Sandwich - R" << fixed << setprecision(2) << SANDWICH_PRICE << endl;
+	cout << "5. Fries - R" << fixed << setprecision(2) << FRIES_PRICE << endl;
+	cout << "6. Drink - R" << fixed << setprecision(2) << DRINK_PRICE << endl;
     
     return 0;
 }
